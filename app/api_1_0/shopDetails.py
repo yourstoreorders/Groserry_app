9d35db8178f438ae10503fa17bab80663047a724
Madhab Sharma <madhabsharma94@gmail.com>
2020-08-11 13:08:50 +0530

    appi respond for shop details

--- app/api_1_0/shopDetails.py
+++ app/api_1_0/shopDetails.py
@@ -7,11 +7,11 @@ from ..models import ShopDetails
 @api.route('/shopdetails/',methods=['GET'])
 def get_shopdetails():
   lists = ShopDetails.query.all()
-  return jsonify({ 'units': [element.to_json() for element in lists] })
+  return jsonify({ 'details': [element.to_json() for element in lists] })
 
 
 @api.route('/shopdetails/<int:id>')
 def get_shopdetail(id):
   element = ShopDetails.query.filter_by(id=id).first()
-  return jsonify({ 'unit': element.to_json() })
+  return jsonify({ 'detail': element.to_json() })
 
