071f61035a63552d7f00ac457e692f10b8e21521
Madhab Sharma <madhabsharma94@gmail.com>
2020-09-28 18:04:19 +0530

    commented out shell context

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db,Admin=Admin ,Product=Product, Unit=Unit,ProductType=ProductType, Stock = Stock, StatusCatalog = StatusCatalog)
 
-manager.add_command("shell", Shell(make_context=make_shell_context))
+# manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 @manager.command
